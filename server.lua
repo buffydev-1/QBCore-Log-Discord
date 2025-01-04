9679f3c332a5d7a4439d38ad91ce4b1df0e438c7
BuffyDev <130619837+buffydev-1@users.noreply.github.com>
2025-01-05 02:24:03 +0300

    server.lua

--- server.lua
+++ server.lua
@@ -3,9 +3,9 @@ local QBCore = exports['qb-core']:GetCoreObject()
 AddEventHandler('playerConnecting', function(name, setCallback, deferrals)
     if Config.LogPlayerJoinLeave then
         local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local logMessage = string.format("[%s] %s is joining the server.\n", timestamp, name)
+        local logMessage = string.format("Player %s is joining the server.", name)
         SaveLog(logMessage)
-        SendToWebhook(logMessage)
+        SendToWebhook(logMessage, 'Player Join', timestamp)
     end
 end)
 
@@ -13,27 +13,27 @@ AddEventHandler('playerDropped', function(reason)
     if Config.LogPlayerJoinLeave then
         local playerName = GetPlayerName(source)
         local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local logMessage = string.format("[%s] %s has left the server. Reason: %s\n", timestamp, playerName, reason)
+        local logMessage = string.format("%s has left the server. Reason: %s", playerName, reason)
         SaveLog(logMessage)
-        SendToWebhook(logMessage)
+        SendToWebhook(logMessage, 'Player Leave', timestamp)
     end
 end)
 
 AddEventHandler('onResourceStart', function(resourceName)
     if Config.LogResourceEvents then
         local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local logMessage = string.format("[%s] Resource '%s' started.\n", timestamp, resourceName)
+        local logMessage = string.format("Resource '%s' started.", resourceName)
         SaveLog(logMessage)
-        SendToWebhook(logMessage)
+        SendToWebhook(logMessage, 'Resource Start', timestamp)
     end
 end)
 
 AddEventHandler('onResourceStop', function(resourceName)
     if Config.LogResourceEvents then
         local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local logMessage = string.format("[%s] Resource '%s' stopped.\n", timestamp, resourceName)
+        local logMessage = string.format("Resource '%s' stopped.", resourceName)
         SaveLog(logMessage)
-        SendToWebhook(logMessage)
+        SendToWebhook(logMessage, 'Resource Stop', timestamp)
     end
 end)
 
@@ -41,9 +41,9 @@ AddEventHandler('txAdmin:BanPlayer', function(playerId, reason, source)
     if Config.LogTxAdminEvents then
         local playerName = GetPlayerName(playerId)
         local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local logMessage = string.format("[%s] Admin banned player %s. Reason: %s\n", timestamp, playerName, reason)
+        local logMessage = string.format("Admin banned player %s. Reason: %s", playerName, reason)
         SaveLog(logMessage)
-        SendToWebhook(logMessage)
+        SendToWebhook(logMessage, 'Player Banned', timestamp)
     end
 end)
 
@@ -51,9 +51,9 @@ AddEventHandler('txAdmin:KickPlayer', function(playerId, reason)
     if Config.LogTxAdminEvents then
         local playerName = GetPlayerName(playerId)
         local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local logMessage = string.format("[%s] Admin kicked player %s. Reason: %s\n", timestamp, playerName, reason)
+        local logMessage = string.format("Admin kicked player %s. Reason: %s", playerName, reason)
         SaveLog(logMessage)
-        SendToWebhook(logMessage)
+        SendToWebhook(logMessage, 'Player Kicked', timestamp)
     end
 end)
 
@@ -61,18 +61,18 @@ AddEventHandler('txAdmin:WarnPlayer', function(playerId, reason)
     if Config.LogTxAdminEvents then
         local playerName = GetPlayerName(playerId)
         local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local logMessage = string.format("[%s] Admin warned player %s. Reason: %s\n", timestamp, playerName, reason)
+        local logMessage = string.format("Admin warned player %s. Reason: %s", playerName, reason)
         SaveLog(logMessage)
-        SendToWebhook(logMessage)
+        SendToWebhook(logMessage, 'Player Warned', timestamp)
     end
 end)
 
 AddEventHandler('txAdmin:ShutdownServer', function()
     if Config.LogTxAdminEvents then
         local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local logMessage = string.format("[%s] Server shutdown initiated by admin.\n", timestamp)
+        local logMessage = string.format("Server shutdown initiated by admin.")
         SaveLog(logMessage)
-        SendToWebhook(logMessage)
+        SendToWebhook(logMessage, 'Server Shutdown', timestamp)
     end
 end)
 
@@ -87,9 +87,9 @@ RegisterCommand('logPlayerAim', function(source, args)
         local distance = #(playerCoords - targetCoords)
         
         local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local logMessage = string.format("[%s] %s aimed at %s with weapon %s at distance %.2f meters.\n", timestamp, playerName, targetPed, weapon, distance)
+        local logMessage = string.format("%s aimed at %s with weapon %s at distance %.2f meters.", playerName, targetPed, weapon, distance)
         SaveLog(logMessage)
-        SendToWebhook(logMessage)
+        SendToWebhook(logMessage, 'Player Aim', timestamp)
     end
 end)
 
@@ -97,15 +97,15 @@ AddEventHandler('explosionEvent', function(coords, creator)
     if Config.LogExplosions then
         local playerName = GetPlayerName(creator)
         local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local logMessage = string.format("[%s] %s caused an explosion at coordinates (%.2f, %.2f, %.2f).\n", timestamp, playerName, coords.x, coords.y, coords.z)
+        local logMessage = string.format("%s caused an explosion at coordinates (%.2f, %.2f, %.2f).", playerName, coords.x, coords.y, coords.z)
         SaveLog(logMessage)
-        SendToWebhook(logMessage)
+        SendToWebhook(logMessage, 'Explosion', timestamp)
 
         TriggerEvent('screenshot:take', coords, function(screenshotData)
             local screenshotPath = SaveScreenshot(screenshotData)
-            logMessage = string.format("[%s] Explosion screenshot saved at: %s\n", timestamp, screenshotPath)
+            logMessage = string.format("Explosion screenshot saved at: %s", screenshotPath)
             SaveLog(logMessage)
-            SendToWebhook(logMessage)
+            SendToWebhook(logMessage, 'Explosion Screenshot', timestamp)
         end)
     end
 end)
@@ -125,9 +125,19 @@ function SaveScreenshot(screenshotData)
     return screenshotPath
 end
 
-function SendToWebhook(message)
+function SendToWebhook(message, eventType, timestamp)
     local data = {
-        content = message
+        embeds = {{
+            title = eventType,
+            description = message,
+            color = 15258703,
+            fields = {
+                { name = 'Timestamp', value = timestamp, inline = true },
+            },
+            footer = {
+                text = 'FiveM Server Logs'
+            }
+        }}
     }
 
     PerformHttpRequest(Config.WebhookURL, function(response)
