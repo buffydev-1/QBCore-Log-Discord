a6305596749dca5ed1bab0ba92b3615a54b781e2
BuffyDev <130619837+buffydev-1@users.noreply.github.com>
2025-01-05 02:18:46 +0300

    config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.EnableLogging = true
+Config.WebhookURL = 'https://your-webhook-url-here'
+Config.LogPlayerJoinLeave = true
+Config.LogResourceEvents = true
+Config.LogTxAdminEvents = true
+Config.LogPlayerAim = true
+Config.LogExplosions = true
+Config.LogFilePath = 'logs.txt'
