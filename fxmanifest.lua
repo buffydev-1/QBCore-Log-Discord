c9c262d7110b909e457d26b09a8a86ff1a2a937e
BuffyDev <130619837+buffydev-1@users.noreply.github.com>
2025-01-05 02:17:27 +0300

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Buffy'
+description 'Custom Multiplayer/Singleplayer Log with advanced events for txAdmin and player actions'
+version '1.0.0'
+
+server_script 'server.lua'
+client_script 'client.lua'
+shared_script 'config.lua'
